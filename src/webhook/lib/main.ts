c7be85bd5ec7912036169626805c94e7fdb4eabd
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2019-12-30 16:37:14 +0100

    Add support for application/x-www-form-urlencoded

--- src/webhook/lib/main.ts
+++ src/webhook/lib/main.ts
@@ -1,14 +1,30 @@
 import { APIGatewayProxyEvent } from 'aws-lambda'
+import querystring from 'querystring'
 
 interface INotificationRepository {
   send: (message: Object) => Promise<any>
 }
 
+// The event we are sending to the EventBridge event bus is similar to APIGatewayProxyEventWithoutBody, body can be string, null or object.
+// This is because we are parsing the request body if the request content type is application/json or application/x-www-form-urlencoded.
+// We are also reusing the APIGatewayProxyEvent interface, but we need to remove body property
+type APIGatewayProxyEventWithoutBody = Omit<APIGatewayProxyEvent, 'body'>
+// And then extend the interface with a new type for a body property
+interface IEvent extends APIGatewayProxyEventWithoutBody {
+  body: string | null | {
+    [key: string]: any
+  }
+}
+
 export async function sendWebhookEvent(event: APIGatewayProxyEvent, notification: INotificationRepository) {
   try {
-    const eventCopy = Object.assign({}, event)
-    if (eventCopy.body && /^application\/json($|;)/.test(eventCopy.headers['Content-Type'])) {
-      eventCopy.body = JSON.parse(eventCopy.body)
+    const eventCopy: IEvent = Object.assign({}, event)
+    if (event.body && /^application\/json($|;)/.test(eventCopy.headers['Content-Type'])) {
+      eventCopy.body = JSON.parse(event.body)
+    }
+
+    if (event.body && /^application\/x-www-form-urlencoded($|;)/.test(eventCopy.headers['Content-Type'])) {
+      eventCopy.body = querystring.parse(event.body)
     }
 
     return await notification.send(eventCopy)
--- src/webhook/tests/main.test.ts
+++ src/webhook/tests/main.test.ts
@@ -92,5 +92,74 @@ describe('Send webhook event', () => {
       expect(notificationMock.send).toHaveBeenCalledTimes(1)
       expect(notificationMock.send).toHaveBeenCalledWith(ApiGwJson)
     })
+
+    it('should parse JSON body if a request Content-Type is application/x-www-form-urlencoded', async () => {
+      const notificationMock = {
+        send: jest.fn(),
+      }
+      const ApiGwJson = Object.assign({}, ApiGwRequest, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, body: 'token=gIkuvaNzQIHg97ATvDxqgjtO&team_id=T0001&team_domain=example&enterprise_id=E0001&enterprise_name=Globular%20Construct%20Inc&channel_id=C2147483705&channel_name=test&user_id=U2147483697&user_name=Steve&command=/weather&text=94070&response_url=https://hooks.slack.com/commands/1234/5678&trigger_id=13345224609.738474920.8088930838d88f008e0' })
+      const ApiGwJsonResponse = Object.assign({}, ApiGwRequest, {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: {
+          token: 'gIkuvaNzQIHg97ATvDxqgjtO',
+          team_id: 'T0001',
+          team_domain: 'example',
+          enterprise_id: 'E0001',
+          enterprise_name: 'Globular Construct Inc',
+          channel_id: 'C2147483705',
+          channel_name: 'test',
+          user_id: 'U2147483697',
+          user_name: 'Steve',
+          command: '/weather',
+          text: '94070',
+          response_url: 'https://hooks.slack.com/commands/1234/5678',
+          trigger_id: '13345224609.738474920.8088930838d88f008e0',
+        },
+      })
+      await sendWebhookEvent(ApiGwJson, notificationMock)
+      expect(notificationMock.send).toHaveBeenCalledTimes(1)
+      expect(notificationMock.send).toHaveBeenCalledWith(ApiGwJsonResponse)
+    })
+
+    it('should parse JSON body if a request Content-Type is application/x-www-form-urlencoded with charset', async () => {
+      const notificationMock = {
+        send: jest.fn(),
+      }
+      const ApiGwJson = Object.assign({}, ApiGwRequest, { headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8' }, body: 'token=gIkuvaNzQIHg97ATvDxqgjtO&team_id=T0001&team_domain=example&enterprise_id=E0001&enterprise_name=Globular%20Construct%20Inc&channel_id=C2147483705&channel_name=test&user_id=U2147483697&user_name=Steve&command=/weather&text=94070&response_url=https://hooks.slack.com/commands/1234/5678&trigger_id=13345224609.738474920.8088930838d88f008e0' })
+      const ApiGwJsonResponse = Object.assign({}, ApiGwRequest, {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8' },
+        body: {
+          token: 'gIkuvaNzQIHg97ATvDxqgjtO',
+          team_id: 'T0001',
+          team_domain: 'example',
+          enterprise_id: 'E0001',
+          enterprise_name: 'Globular Construct Inc',
+          channel_id: 'C2147483705',
+          channel_name: 'test',
+          user_id: 'U2147483697',
+          user_name: 'Steve',
+          command: '/weather',
+          text: '94070',
+          response_url: 'https://hooks.slack.com/commands/1234/5678',
+          trigger_id: '13345224609.738474920.8088930838d88f008e0',
+        },
+      })
+      await sendWebhookEvent(ApiGwJson, notificationMock)
+      expect(notificationMock.send).toHaveBeenCalledTimes(1)
+      expect(notificationMock.send).toHaveBeenCalledWith(ApiGwJsonResponse)
+    })
+
+    it('should fall back to the original body if parsing querystring fails', async () => {
+      const notificationMock = {
+        send: jest.fn(),
+      }
+      const ApiGwJson = Object.assign({}, ApiGwRequest, {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8' },
+        body: '',
+      })
+      await sendWebhookEvent(ApiGwJson, notificationMock)
+      expect(notificationMock.send).toHaveBeenCalledTimes(1)
+      expect(notificationMock.send).toHaveBeenCalledWith(ApiGwJson)
+    })
   })
 })
